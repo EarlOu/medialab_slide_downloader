9328d460400542a7970cf746b111df460e121297
Shun-Hsing Ou <occqoo@gmail.com>
2012-09-20 12:11:46 +0800

    query the data

--- src/com/media/medialabpttdownload/MainActivity.java
+++ src/com/media/medialabpttdownload/MainActivity.java
@@ -1,19 +1,29 @@
 package com.media.medialabpttdownload;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthenticationException;
 import org.apache.http.auth.Credentials;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.auth.BasicScheme;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -30,13 +40,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
 import android.widget.EditText;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
     private static final String TAG = "MainActivity";
 
     private static final String LOGIN_URL =
             "http://media.ee.ntu.edu.tw/group_meeting_ppt/web_search.php";
+    private static final String PPT_URL =
+            "http://media.ee.ntu.edu.tw/group_meeting_ppt/test_php/searchPPTu.php";
 
     private static final String PREF = "pref";
     private static final String PREF_USERNAME = "username";
@@ -53,6 +64,17 @@ public class MainActivity extends Activity {
     }
 
     private Dialog mLoginDialog;
+    private Dialog mRetryDialog;
+    private String mUsername = "";
+    private String mPassword = "";
+    private String mPresenter = "*";
+    private String mType = "*";
+    private String mTitle = "";
+    private String mDateFrom = "2000-01-01";
+    private String mDateTo = "2100-08-31";
+    private String mKeyword = "";
+    private int mPageSize = 10;
+    private int mStartIndex = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -80,12 +102,13 @@ public class MainActivity extends Activity {
                 , new OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
+                mUsername = username.getText().toString();
+                mPassword = password.getText().toString();
                 SharedPreferences.Editor editor = pref.edit();
-                editor.putString(PREF_USERNAME, username.getText().toString());
-                editor.putString(PREF_PASSWORD, password.getText().toString());
+                editor.putString(PREF_USERNAME, mUsername);
+                editor.putString(PREF_PASSWORD, mPassword);
                 editor.commit();
-                login(username.getText().toString()
-                        , password.getText().toString());
+                query();
             }
         });
         builder.setOnCancelListener(new OnCancelListener() {
@@ -97,9 +120,27 @@ public class MainActivity extends Activity {
         mLoginDialog = builder.create();
     }
 
-    private void login(final String username, final String password) {
-        new AsyncTask<Void, Void, Boolean> () {
+    private static final String ENCODING_TYPE = "utf-8";
+
+    private void query() {
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+        nameValuePairs.add(new BasicNameValuePair("presenterPat", mPresenter));
+        nameValuePairs.add(new BasicNameValuePair("typePat", mType));
+        nameValuePairs.add(new BasicNameValuePair("titlePat", mTitle));
+        nameValuePairs.add(new BasicNameValuePair("datefrom", mDateFrom));
+        nameValuePairs.add(new BasicNameValuePair("dateto", mDateTo));
+        nameValuePairs.add(new BasicNameValuePair("semfrom", mDateFrom));
+        nameValuePairs.add(new BasicNameValuePair("semto", mDateTo));
+        nameValuePairs.add(new BasicNameValuePair("keywordPat", mKeyword));
+        nameValuePairs.add(new BasicNameValuePair("pageSize", String.valueOf(mPageSize)));
+        nameValuePairs.add(new BasicNameValuePair("startIndex",
+                String.valueOf(mStartIndex)));
+        String url = PPT_URL + "?" + URLEncodedUtils.format(nameValuePairs, ENCODING_TYPE);
+        final HttpGet httpGet = new HttpGet(url);
+
+        new AsyncTask<Void, Void, Integer>() {
             private ProgressDialog progress;
+            private String resBody;
             @Override
             protected void onPreExecute() {
                 progress = ProgressDialog.show(MainActivity.this
@@ -112,40 +153,83 @@ public class MainActivity extends Activity {
                     }
                 });
             }
+
             @Override
-            protected void onPostExecute(Boolean result) {
-                progress.dismiss();
-                if (result) {
-                    loginSuccess();
-                } else {
-                    Toast.makeText(MainActivity.this, R.string.login_error
-                            , Toast.LENGTH_SHORT).show();
-                    mLoginDialog.show();
-                }
-            }
-            @Override
-            protected Boolean doInBackground(Void... params) {
+            protected Integer doInBackground(Void... params) {
                 try {
-                    HttpGet httpGet = new HttpGet(LOGIN_URL);
                     Credentials creds = new UsernamePasswordCredentials(
-                            username, password);
+                            mUsername, mPassword);
                     Header header = new BasicScheme().authenticate(creds, httpGet);
                     httpGet.setHeader(header);
                     HttpResponse response = mClient.execute(httpGet);
-                    return response.getStatusLine().getStatusCode() == 200;
-                } catch (ClientProtocolException e) {
-                    Log.e(TAG, "", e);
-                } catch (IOException e) {
-                    Log.e(TAG, "", e);
+                    resBody = EntityUtils.toString(response.getEntity(), ENCODING_TYPE);
+                    return response.getStatusLine().getStatusCode();
                 } catch (AuthenticationException e) {
-                    Log.e(TAG, "", e);
+                    Log.e(TAG, "",e);
+                } catch (ClientProtocolException e) {
+                    Log.e(TAG, "",e);
+                } catch (IOException e) {
+                    Log.e(TAG, "",e);
                 }
-                return false;
+                return 0;
+            }
+
+            @Override
+            protected void onPostExecute(Integer code) {
+                progress.dismiss();
+                if (code == 200) {
+                    handleQueryResponse(resBody);
+                } else if (code == 401) {
+                    mLoginDialog.show();
+                } else {
+                    showRetryDialog();
+                }
             }
         }.execute();
     }
 
-    private void loginSuccess() {
+    private void handleQueryResponse(String resBody) {
+        Document doc = Jsoup.parse(resBody);
+        Element table = doc.select("tbody").first();
+        Elements list = table.children();
+        for (int i = 0, n = list.size(); i < n; i++) {
+            if(i % 2 == 0) continue;
+            Element item = list.get(i);
+            Elements data = item.children();
+            String time = data.get(0).text();
+            String type = data.get(1).text();
+            String title = data.get(2).child(0).text();
+            String presenter = data.get(3).text();
+            String link = data.get(4).child(0).attr("href");
+            Log.e(TAG, "Time: " + time);
+            Log.e(TAG, "Type: " + type);
+            Log.e(TAG, "Title: " + title);
+            Log.e(TAG, "Presenter: " + presenter);
+            Log.e(TAG, "Link: " + link);
+        }
+    }
 
+    private void showRetryDialog() {
+        if (mRetryDialog != null) {
+            mRetryDialog.show();
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getResources().getString(R.string.network_error));
+        builder.setPositiveButton(R.string.retry, new OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int button) {
+                query();
+            }
+        });
+        builder.setCancelable(true);
+        builder.setOnCancelListener(new OnCancelListener() {
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                finish();
+            }
+        });
+        mRetryDialog = builder.create();
+        mRetryDialog.show();
     }
 }
